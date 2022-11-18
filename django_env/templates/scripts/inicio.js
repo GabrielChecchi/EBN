7c279d1325d10d06568da92d80d7ccdfa93a6685
Txmz <cesartomas2003@gmail.com>
2022-11-18 13:46:02 -0300

    Separación de footer y otros

--- django_env/templates/scripts/inicio.js
+++ django_env/templates/scripts/inicio.js
@@ -2,9 +2,9 @@ function cambiarMegusta() {
   let elem = document.getElementById('imagen');
   var atrib = elem.getAttribute("src");
 
-  if (atrib=="/img/megusta_activado.png"){
-      elem.setAttribute("src", "/img/megusta_desactivado.png")
+  if (atrib=="img/megusta_activado.png"){
+      elem.setAttribute("src", "img/megusta_desactivado.png")
   } else{
-      elem.setAttribute("src", "/img/megusta_activado.png")
+      elem.setAttribute("src", "img/megusta_activado.png")
   }
 }
